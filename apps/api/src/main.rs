23f193cd8b6c461063537416c7a24b728874d824
Hirofumi Omi <hirofumi.omi@3-shake.com>
2025-03-09 20:49:09 +0900

    feat:http server

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,1 +1,17 @@
+use actix_web::{middleware, App, HttpServer};
+use household_finance_api::api::transactions::configure_routes;
 
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    log::info!("starting HTTP server at http://localhost:8080");
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .configure(configure_routes)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
